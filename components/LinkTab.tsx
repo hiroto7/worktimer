d7195351786f46ab8ceded75e21e1a8ed7db6868
hiroto7 <hiroto.dev@gmail.com>
2023-07-02 17:58:33 +0900

    Disable "Recent" tab when history is empty

--- components/LinkTab.tsx
+++ components/LinkTab.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import type { ComponentProps } from "react";
 
 export const LinkTab: React.FC<
-  Pick<ComponentProps<typeof Tab>, "value" | "icon" | "label">
+  Pick<ComponentProps<typeof Tab>, "value" | "icon" | "label" | "disabled">
 > = ({ value, ...props }) => (
   <Tab {...props} component={Link} value={value} href={value} />
 );
--- components/MyTabs.tsx
+++ components/MyTabs.tsx
@@ -3,10 +3,19 @@
 import { History, Home } from "@mui/icons-material";
 import { LinkTab } from "./LinkTab";
 import { LinkTabs } from "./LinkTabs";
+import { useRecentTasks } from "@/lib/hooks/use-recent-tasks";
 
-export const MyTabs: React.FC = () => (
-  <LinkTabs centered>
-    <LinkTab value="/" icon={<Home />} label="Home" />
-    <LinkTab value="/recent" icon={<History />} label="Recent" />
-  </LinkTabs>
-);
+export const MyTabs: React.FC = () => {
+  const { tasks } = useRecentTasks();
+  return (
+    <LinkTabs centered>
+      <LinkTab value="/" icon={<Home />} label="Home" />
+      <LinkTab
+        value="/recent"
+        icon={<History />}
+        label="Recent"
+        disabled={tasks.size === 0}
+      />
+    </LinkTabs>
+  );
+};
