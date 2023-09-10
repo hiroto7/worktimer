59996b72f3fce812804531a3de651bb6d532c917
hiroto7 <hiroto.dev@gmail.com>
2023-09-10 22:17:53 +0900

    Disable `/history` page when no events are triggered

--- components/MyTabs.tsx
+++ components/MyTabs.tsx
@@ -4,9 +4,12 @@ import { useRecentTasks } from "@/lib/hooks/use-recent-tasks";
 import { History, Home, Timeline } from "@mui/icons-material";
 import { LinkTab } from "./LinkTab";
 import { LinkTabs } from "./LinkTabs";
+import { useTaskEvents } from "@/lib/hooks/use-task-events";
 
 export const MyTabs: React.FC = () => {
   const { tasks } = useRecentTasks();
+  const { events } = useTaskEvents();
+
   return (
     <LinkTabs centered>
       <LinkTab value="/" icon={<Home />} label="Home" />
@@ -20,7 +23,7 @@ export const MyTabs: React.FC = () => {
         value="/history"
         icon={<Timeline />}
         label="History"
-        disabled={tasks.length === 0}
+        disabled={events.length === 0}
       />
     </LinkTabs>
   );
