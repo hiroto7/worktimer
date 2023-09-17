b19d73ebc5525b473cebc72d788f9d2582599666
hiroto7 <hiroto.dev@gmail.com>
2023-09-17 20:40:35 +0900

    Add swap button

--- components/TimeTransferDialog.tsx
+++ components/TimeTransferDialog.tsx
@@ -1,12 +1,14 @@
 import { OngoingTaskElapsedTimeParams, Task } from "@/lib";
 import { useElapsedTime } from "@/lib/hooks/use-elapsed-time";
 import { useTaskEvents } from "@/lib/hooks/use-task-events";
+import { SwapHoriz, SwapVert } from "@mui/icons-material";
 import {
   Button,
   Dialog,
   DialogActions,
   DialogContent,
   FormControl,
+  IconButton,
   InputLabel,
   ListItemText,
   MenuItem,
@@ -14,6 +16,7 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import { useState } from "react";
 import { FormattedTime } from "./FormattedTime";
 
@@ -62,46 +65,69 @@ export const TimeTransferDialog: React.FC<{
         setFrom(defaultFrom ?? "");
         setTo("");
       }}
+      fullWidth
     >
       <DialogContent>
-        <FormControl margin="normal" fullWidth>
-          <InputLabel>From</InputLabel>
-          <Select
-            label="From"
-            fullWidth
-            value={from}
-            onChange={({ target: { value } }) => setFrom(value)}
-          >
-            {tasks.map(({ name, uuid }) => (
-              <MenuItem value={uuid} key={uuid} disabled={uuid === to}>
-                <MenuItemContent
-                  previousElapsedTime={elapsedTimes.get(uuid) ?? 0}
-                  ongoing={ongoingTasks.has(uuid) ? ongoing : undefined}
-                  name={name}
-                />
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl margin="normal" fullWidth>
-          <InputLabel>To</InputLabel>
-          <Select
-            autoFocus
-            label="To"
-            value={to}
-            onChange={({ target: { value } }) => setTo(value)}
-          >
-            {tasks.map(({ name, uuid }) => (
-              <MenuItem value={uuid} key={uuid} disabled={uuid === from}>
-                <MenuItemContent
-                  previousElapsedTime={elapsedTimes.get(uuid) ?? 0}
-                  ongoing={ongoingTasks.has(uuid) ? ongoing : undefined}
-                  name={name}
-                />
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+        <Grid
+          container
+          spacing={{ sm: 1 }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid xs={12} sm>
+            <FormControl margin="normal" fullWidth>
+              <InputLabel>From</InputLabel>
+              <Select
+                label="From"
+                fullWidth
+                value={from}
+                onChange={({ target: { value } }) => setFrom(value)}
+              >
+                {tasks.map(({ name, uuid }) => (
+                  <MenuItem value={uuid} key={uuid} disabled={uuid === to}>
+                    <MenuItemContent
+                      previousElapsedTime={elapsedTimes.get(uuid) ?? 0}
+                      ongoing={ongoingTasks.has(uuid) ? ongoing : undefined}
+                      name={name}
+                    />
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid xs="auto">
+            <IconButton
+              onClick={() => {
+                setFrom(to);
+                setTo(from);
+              }}
+            >
+              <SwapHoriz sx={{ display: { xs: "none", sm: "block" } }} />
+              <SwapVert sx={{ display: { sm: "none" } }} />
+            </IconButton>
+          </Grid>
+          <Grid xs={12} sm>
+            <FormControl margin="normal" fullWidth>
+              <InputLabel>To</InputLabel>
+              <Select
+                autoFocus
+                label="To"
+                value={to}
+                onChange={({ target: { value } }) => setTo(value)}
+              >
+                {tasks.map(({ name, uuid }) => (
+                  <MenuItem value={uuid} key={uuid} disabled={uuid === from}>
+                    <MenuItemContent
+                      previousElapsedTime={elapsedTimes.get(uuid) ?? 0}
+                      ongoing={ongoingTasks.has(uuid) ? ongoing : undefined}
+                      name={name}
+                    />
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+        </Grid>
         <TextField
           type="number"
           autoFocus
