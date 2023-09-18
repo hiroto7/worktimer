8c82f72f30a96c6663a8f2edba80acfc47522d52
hiroto7 <hiroto.dev@gmail.com>
2023-09-18 16:39:09 +0900

    Add `eslint:recommended` and `@typescript-eslint`

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "next/core-web-vitals",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  root: true,
+};
